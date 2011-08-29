e6fd8f2e45d317995df1c85f2e6ca64d82079c6d
Christoph Heer <Christoph.Heer@googlemail.com>
2011-08-29 14:56:09 +0200

    Add tags module

---
+++ flaskrst/modules/tags/__init__.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+    flask-rst.modules.tags
+    ~~~~~~~~~~~~~~~~~~~~~~
+    
+    :copyright: (c) 2011 by Christoph Heer.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from math import log
+
+from flask import Blueprint, render_template
+from jinja2 import Markup
+from flaskrst.modules.blog import get_posts
+
+def get_tags():
+    tags = {}
+    for post in get_posts():
+        post_tags = [tag.lower() for tag in post.config.get('tags', [])]
+        for tag in post_tags:
+            if tag not in tags:
+                tags[tag] = 1
+            else:
+                tags[tag] += 1
+    return tags
+
+def get_posts_by_tag(name):
+    posts = []
+    for post in get_posts():
+        post_tags = [tag.lower() for tag in post.config.get('tags', [])]
+        for tag in post_tags:
+            if tag == name and post not in posts:
+                posts.append(post)
+    return posts
+
+def template_tags(doc):
+    tags = [tag.lower() for tag in doc.config.get('tags', [])]
+    return Markup(render_template('post_tags.html', tags=tags))
+
+tags = Blueprint('tags', __name__, template_folder='templates')
+
+@tags.route("/tags")
+def cloud():
+    tags = get_tags()
+    for tag in tags:
+        tags[tag] = 100 + log(tags[tag] or 1) * 20
+    return render_template('cloud.html',
+        tags=tags
+    )
+    
+@tags.route("/tags/<tag>")
+def tag(tag):
+    blog_posts = get_posts_by_tag(tag)
+    return render_template('tag.html',
+        tag=tag,
+        blog_posts=blog_posts
+    )
+
+def setup(app, cfg):
+    app.jinja_env.globals['tags'] = template_tags
+    app.register_blueprint(tags)
