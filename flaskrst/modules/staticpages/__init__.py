1c0302c7137af550314b05f6e3cc87134c9bdc65
Christoph Heer <Christoph.Heer@googlemail.com>
2011-09-03 12:03:57 +0200

    Rename blueprint static_pages to staticpages and rename staticpages.show to staticpages.page

--- flaskrst/modules/staticpages/__init__.py
+++ flaskrst/modules/staticpages/__init__.py
@@ -7,22 +7,19 @@
     :license: BSD, see LICENSE for more details.
 """
 
-import os
-
-from flask import Blueprint, current_app, render_template
+from flask import Blueprint, current_app, render_template, safe_join
 from flaskrst.parsers import rstDocument
 
-static_pages = Blueprint('static_pages', __name__, \
-                         template_folder='templates')
+staticpages = Blueprint('staticpages', __name__, template_folder='templates')
 
-@static_pages.route('/', defaults={'file_path': 'index'})
-@static_pages.route('/<path:file_path>')
-def show(file_path):
+@staticpages.route('/', defaults={'file_path': 'index'})
+@staticpages.route('/<path:file_path>')
+def page(file_path):
     if file_path.endswith('/'):
         file_path += "index"
-    rst_file = os.path.join(current_app.config['SOURCE'], file_path + '.rst')
+    rst_file = safe_join(current_app.config['SOURCE'], file_path + '.rst')
     rst = rstDocument(rst_file)
     return render_template("static_page.html", page=rst)
     
 def setup(app, cfg):
-    app.register_blueprint(static_pages)
+    app.register_blueprint(staticpages)
